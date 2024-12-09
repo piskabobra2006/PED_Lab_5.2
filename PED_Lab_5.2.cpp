5de07cd3e0f33548ecc07a27fdf2ab21e915b818
Pavlo-kik <pavhunskysuslo@gmail.com>
2024-12-09 12:09:15 +0200

    Add files via upload

---
+++ PED_Lab_5.2.cpp
@@ -0,0 +1,58 @@
+#include <iostream>
+#include <iomanip>
+#include <cmath>
+
+using namespace std;
+void S(const double x, const double eps, int n, double s);
+void A(const double x, const int n, double& a);
+
+int main()
+{
+    double xp, xk, x, dx, eps, s = 0;
+    int n = 0;
+    cout << "xp = "; cin >> xp;
+    cout << "xk = "; cin >> xk;
+    cout << "dx = "; cin >> dx;
+    cout << "eps = "; cin >> eps;
+    cout << fixed;
+    cout << "-------------------------------------------------" << endl;
+    cout << "|" << setw(5) << "x" << " |"
+        << setw(10) << "exp(x)" << " |"
+        << setw(7) << "S" << " |"
+        << setw(5) << "n" << " |"
+        << endl;
+    cout << "-------------------------------------------------" << endl;
+    x = xp;
+    while (x <= xk)
+    {
+        s = (x, eps, n, s);
+        cout << "|" << setw(7) << setprecision(2) << x << " |"
+            << setw(10) << setprecision(5) << exp(x) << " |"
+            << setw(10) << setprecision(5) << s << " |"
+            << setw(5) << n << " |"
+            << endl;
+        x += dx;
+    }
+    cout << "-------------------------------------------------" << endl;
+    return 0;
+}
+
+double s(const double x, const double eps, int n, double s)
+{
+    n = 0;
+    double a = x;
+    s = a;
+    do {
+        n++;
+        a = (x, n, a);
+        s += a;
+    } while (abs(a) >= eps);
+    return s;
+}
+
+double A(const double x, const int n, double a)
+{
+    double R = ((x * x) * ((2 * n - 1.0) / (2 * n + 1.0)));
+    a *= R;
+    return a;
+}
